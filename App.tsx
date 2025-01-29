fd819530dff0e0a70d33c74ae23f79598fb44943
Silas Roberto Mariosa de Vasconcelos <113197484+silas-mariosa@users.noreply.github.com>
2025-01-29 13:18:28 -0300

    chore: add asyncStorage

--- App.tsx
+++ App.tsx
@@ -4,23 +4,48 @@ import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { useState } from 'react';
 import AddAgenda from './components/AddAgenda';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect } from 'react';
 
-const agenda = [
-  {
-    id: 1,
-    agenda: 'Clique em + para adicionar uma nova tarefa',
+interface Tarefa {
+  id: number,
+  agenda: string
+}
+
+const saveTasks = async (tarefas: Tarefa[]) => {
+  try {
+    const jsonTarefas = JSON.stringify(tarefas);
+    await AsyncStorage.setItem('@tarefas', jsonTarefas);
+  } catch (error) {
+    console.error("Erro ao salvar tarefas:", error);
   }
-]
+};
 
-
+// Função para carregar as tarefas ao iniciar o app
+const loadTasks = async () => {
+  try {
+    const jsonTarefas = await AsyncStorage.getItem('@tarefas');
+    return jsonTarefas ? JSON.parse(jsonTarefas) : [];
+  } catch (error) {
+    console.error("Erro ao carregar tarefas:", error);
+    return [];
+  }
+}
 
 export default function App() {
   const [fontLato] = useFonts({
     Lato_400Regular,
   });
-
-  const [tarefas, setTarefas] = useState(agenda)
+  const [tarefas, setTarefas] = useState<Tarefa[]>([])
   const [modalVisible, setModalVisible] = useState(false)
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const tarefasSalvas = await loadTasks();
+      setTarefas(tarefasSalvas);
+    };
+    fetchTasks();
+  })
 
   if (!fontLato) {
     return (
@@ -33,6 +58,7 @@ export default function App() {
   function deletarTarefa(id: number) {
     const novasTarefas = tarefas.filter((item) => item.id !== id)
     alert('Tarefa deletada com sucesso!')
+    saveTasks(novasTarefas)
     setTarefas(novasTarefas)
   }
 
@@ -41,7 +67,9 @@ export default function App() {
       id: Math.random(),
       agenda: desricao
     }
+    const novasTarefas = [...tarefas, novaTarefa]
     setTarefas([...tarefas, novaTarefa])
+    saveTasks(novasTarefas)
   }
   return (
     <View style={{ flex: 1, backgroundColor: '#9ACBD0' }}>
@@ -53,18 +81,23 @@ export default function App() {
       </View>
       <ScrollView style={{ flex: 1, backgroundColor: '#9ACBD0' }}>
         {
-          tarefas.map((item) => (
-            <View key={item.id} style={{ margin: 5, borderRadius: 15, width: 'auto', borderBottomWidth: 1, borderBottomColor: '#000', backgroundColor: '#fff', flexDirection: 'row', padding: 15, justifyContent: 'space-between', alignItems: 'center' }}>
-              <View>
-                <Text style={{ fontSize: 14, color: '#000', fontFamily: 'Lato_400Regular' }}>{item.agenda}</Text>
-              </View>
-              <View>
-                <TouchableOpacity onPress={() => { deletarTarefa(item.id) }}>
-                  <AntDesign name="minuscircleo" size={24} color="black" />
-                </TouchableOpacity>
-              </View>
+          (tarefas.length === 0) ? (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+              <Text style={{ fontSize: 18, color: '#fff', fontFamily: 'Lato_400Regular' }}>Nenhuma tarefa cadastrada!</Text>
             </View>
-          ))
+          ) :
+            tarefas.map((item) => (
+              <View key={item.id} style={{ margin: 5, borderRadius: 15, width: 'auto', borderBottomWidth: 1, borderBottomColor: '#000', backgroundColor: '#fff', flexDirection: 'row', padding: 15, justifyContent: 'space-between', alignItems: 'center' }}>
+                <View>
+                  <Text style={{ fontSize: 14, color: '#000', fontFamily: 'Lato_400Regular' }}>{item.agenda}</Text>
+                </View>
+                <View>
+                  <TouchableOpacity onPress={() => { deletarTarefa(item.id) }}>
+                    <AntDesign name="minuscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ))
         }
       </ScrollView>
       <View>
